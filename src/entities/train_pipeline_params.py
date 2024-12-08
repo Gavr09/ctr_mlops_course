6ad5f3d9240c7d4f180646f39905e120c18fa2b3
Gavr09 <ng062974@gmail.com>
2024-12-08 13:05:14 +0300

    The service for model was created and wrapped in docker container

--- src/entities/train_pipeline_params.py
+++ src/entities/train_pipeline_params.py
@@ -22,8 +22,10 @@ PATH = "configs/train_config.yaml"
 
 @dataclass()
 class TrainingPipelineParams:
+    input_preprocessed_data_path: str
     output_model_path: str
     output_transformer_path: str
+    output_ctr_transformer_path: str
     metric_path: str
     splitting_params: SplittingParams
     feature_params: FeatureParams
